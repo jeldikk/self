52cfcfff99493ac62ab66d9951802c62cf39b3d7
jeldikk <jeldi.kamal2013@gmail.com>
2020-12-24 14:42:19 +0530

    Changed breakpoint to 767.98px and good view for mobile and laptops

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 import {Container, Navbar, Nav} from 'react-bootstrap'
 
+import LogoImage from "../../assets/logo.png"
+
+import "./header.styles.scss"
+
 const Header = () => {
     return (
         <header className="header">
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">@jeldikk</Navbar.Brand>
+                    <Navbar.Brand href="/"><img className="logo_image rounded-circle" src={LogoImage} /> @jeldikk</Navbar.Brand>
                     <Nav className="ml-auto">
                         <Nav.Link href="https://github.com/jeldikk">Repository</Nav.Link>
-                        {/* <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link> */}
                     </Nav>
                 </Container>
             </Navbar>
