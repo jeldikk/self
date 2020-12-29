8444a2559f8377e91c8ba51b16c7fa2b87159400
jeldikk <jeldi.kamal2013@gmail.com>
2020-12-29 12:08:29 +0530

    added seperate css for landscape and potrait modes

--- src/components/title-card/title-card.component.jsx
+++ src/components/title-card/title-card.component.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import "./title-card.styles.scss"
+import "./title-card.styles.scss";
 
 const TitleCard = () => {
-    return (
-        <div className='title-card text-center'>
-            <div className="title-name"><span className="initial">J</span> <span className="firstName">Kamal</span> <span className="lastName">Kumar</span></div>
-            <p className="caption">An Engineer by Profession and Web Developer by Passion</p>
-        </div>
-    )
-}
+  return (
+    <div className="title-card text-center">
+      <div className="title-name">
+        <span className="initial">J</span>{" "}
+        <span className="firstName">Kamal</span>{" "}
+        <span className="lastName">Kumar</span>
+      </div>
+      <p className="caption">
+        An Engineer by Profession and Web Developer by Passion
+      </p>
+    </div>
+  );
+};
 
-export default TitleCard
+export default TitleCard;
