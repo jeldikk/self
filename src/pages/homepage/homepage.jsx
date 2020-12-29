9b0a8206cbf986c9d73863c9bd72a4f8d5ea1a02
jeldikk <jeldi.kamal2013@gmail.com>
2020-12-29 12:09:33 +0530

    added bootstrap sass mixins for different potrait and landscape modes

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 import ImageRoller from "../../components/image-roller/image-roller.component"
 import TitleCard from "../../components/title-card/title-card.component"
 
+import "./homepage.styles.scss"
+
 const Homepage = () => {
     return (
         <div className="homepage">
             <ImageRoller />
             <TitleCard />
+            
         </div>
     )
 }
